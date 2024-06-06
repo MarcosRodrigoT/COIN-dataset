1fe6c1da9bf071e3cd125b4f81ed33983a5d95df
MarcosRodrigoT <marcosrodrigo5@hotmail.com>
2024-06-06 21:25:06 +0200

    Create script to download transcripts

---
+++ download_transcripts.py
@@ -0,0 +1,28 @@
+## Pre-requisities: run 'pip install youtube-dl' to install the youtube-dl package.
+## Specify your location of output videos and input json file.
+import json
+import os
+
+output_path = "./transcripts"
+json_path = "./COIN.json"
+
+if not os.path.exists(output_path):
+    os.mkdir(output_path)
+
+data = json.load(open(json_path, "r"))["database"]
+youtube_ids = list(data.keys())
+
+for youtube_id in data:
+    info = data[youtube_id]
+    type = info["recipe_type"]
+    url = info["video_url"]
+    trans_loc = output_path + "/" + str(type)
+    if not os.path.exists(trans_loc):
+        os.mkdir(trans_loc)
+
+    cmd = f"yt-dlp --write-auto-sub --skip-download --sub-format 'srt' --sub-lang 'en' -o '{trans_loc}/{youtube_id}' {url}"
+    os.system(cmd)
+
+    # To save disk space, you could download the best format available
+    #   but not better that 480p or any other qualities optinally
+    # See https://askubuntu.com/questions/486297/how-to-select-video-quality-from-youtube-dl
